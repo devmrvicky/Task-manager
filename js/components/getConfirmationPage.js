798e1b45eca9151dfe69bfd93cf859d2a8001ced
Vikash Kumar <mrvikashkumar396@gmail.com>
2023-09-05 07:12:01 +0530

    init note page

---
+++ js/components/getConfirmationPage.js
@@ -0,0 +1,26 @@
+export const getConfirmationPage = () => {
+  const container = document.createElement("div");
+  container.className =
+    "container w-screen h-full fixed top-0 right-0 bg-black/20 z-40 flex justify-center items-center";
+  const box = document.createElement("div");
+  box.className =
+    "confirmation-box max-w-[400px] h-[200px] border bg-white flex items-center flex-col gap-10 p-10 rounded-lg";
+  box.innerHTML = `
+    <h1>Do you want to delete your all tasks?</h1>
+    <div class="w-full flex justify-center gap-10">
+      <button class="border px-3 py-2 bg-red-400 rounded hover:bg-red-500 text-white" id="delete-tasks">
+        delete
+      </button>
+      <button class="border px-3 py-2 rounded hover:bg-zinc-100" id="cancel">
+        cancel
+      </button>
+    </div>
+  `;
+  container.append(box);
+  container.addEventListener("click", (e) => {
+    if (e.target.classList.contains("container")) {
+      container.remove();
+    }
+  });
+  return container;
+};
