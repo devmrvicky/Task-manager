82dabcfbcb9db4316a8b0df5e31cec17ffb430ff
devmrvicky <devmrvicky@gmail.com>
2023-09-14 07:26:48 +0530

    tailwind configuration completed

--- js/components/Text-editor/getNotesWritingArea.js
+++ js/components/Text-editor/getNotesWritingArea.js
@@ -2,10 +2,10 @@ export const getNotesWritingArea = () => {
   const container = document.createElement("div");
   container.className = `writing-area-container flex-1 w-full overflow-auto bg-[#F9FBFD] flex justify-center py-3`;
   const writingArea = document.createElement("div");
-  let width = `700`;
-  writingArea.className = `writing-area p-10 border bg-white max-w-[${width}px] w-[97%] h-[${
-    1.414 * width
-  }px] outline-none`;
+  let width = 700;
+  let height = 1.414 * width;
+  writingArea.className = `writing-area p-10 border bg-white max-w-[${width}px] w-[97%] outline-none`;
+  writingArea.style.height = height + "px";
   writingArea.setAttribute("contenteditable", "true");
   // writingArea.setAttribute("draggable", "true");
   writingArea.appendChild(
